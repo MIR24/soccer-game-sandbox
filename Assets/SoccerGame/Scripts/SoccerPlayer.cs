38b0b2b6c28fda1b9534a6c719cc265a5849d3d1
Dmitri Yourchev <d.yurchev@yandex.ru>
2018-04-12 11:04:00 +0300

    Add boy turn for target watching procedure

--- Assets/SoccerGame/Scripts/SoccerPlayer.cs
+++ Assets/SoccerGame/Scripts/SoccerPlayer.cs
@@ -10,7 +10,17 @@ public class SoccerPlayer : MonoBehaviour {
     public float moveSpeed = 0;
     public float moveDirection = 0;
     public bool abruptStop = false;
+    public bool turnToTarget = false;
     public GameObject myTarget;
+    public Vector3 myTargetDirection;
+    public float myTargetDirectionAngle;
+    public float myTargetDirectionAngleCorrected;
+    public float maxFieldOfView = 200F;
+    public float visionAngle = 30F;
+    public float forwardAngleCorrection = 90F;
+    public float turnToRightAngle;
+    public bool turnToLeft;
+    public bool turnToRight;
 
     private bool controllingBall;
     private Vector3 dribblingDirection;
@@ -19,6 +29,8 @@ public class SoccerPlayer : MonoBehaviour {
     private int moveSpeedHash;
     private int moveDirectionHash;
     private int abruptStopHash;
+    private int turnLeftInPlaceHash;
+    private int turnRightInPlaceHash;
     
 	// Use this for initialization
 	void Start () {
@@ -30,6 +42,8 @@ public class SoccerPlayer : MonoBehaviour {
         moveSpeedHash = Animator.StringToHash("Speed");
         moveDirectionHash = Animator.StringToHash("Direction");
         abruptStopHash = Animator.StringToHash("AbruptStop");
+        turnLeftInPlaceHash = Animator.StringToHash("TurnLeftInPlace");
+        turnRightInPlaceHash = Animator.StringToHash("TurnRightInPlace");
 
         //Setup Listeners
         MessageDispatcher.AddListener("ball_controlled_by", this.gameObject.name, takeBallControl, true);
@@ -43,14 +57,42 @@ public class SoccerPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //Manage Animator States
+        //Setup movement speed and direction
         anim.SetFloat(moveSpeedHash, moveSpeed);
         anim.SetFloat(moveDirectionHash, moveDirection);
+        
+        //Flush animation flags
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Left_turn_in_place") && anim.GetBool(turnLeftInPlaceHash)) {
+            anim.SetBool(turnLeftInPlaceHash, false);
+        }
+
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Right_turn_in_place") && anim.GetBool(turnRightInPlaceHash)) {
+            anim.SetBool(turnRightInPlaceHash, false);
+        }
+
+        //Trigger abrupt stop during sprint
         if (abruptStop && !anim.GetBool(abruptStopHash))
         {
             anim.SetBool(abruptStopHash, true);
         }
         if (!abruptStop && anim.GetBool(abruptStopHash)) anim.SetBool(abruptStopHash, false);
+
+        //Calculate target heading angle
+        myTargetDirection = myTarget.transform.position - transform.position;
+        myTargetDirectionAngle = Vector3.SignedAngle(myTargetDirection, transform.forward, Vector3.up);
+
+        //Check if should turn left
+        if (myTargetDirectionAngle > maxFieldOfView / 2 + visionAngle && !anim.GetBool(turnLeftInPlaceHash)) {
+            Debug.Log("Have to turn left");
+            anim.SetBool(turnLeftInPlaceHash, true);
+        }
+        //Check if should turn right
+        if (myTargetDirectionAngle < -maxFieldOfView / 2 - visionAngle && !anim.GetBool(turnRightInPlaceHash)) {
+            Debug.Log("Have to turn right");
+            anim.SetBool(turnRightInPlaceHash, true);
+        }
+
+        Debug.Log("Do I turning in place left" + anim.GetCurrentAnimatorStateInfo(0).IsName("Left_turn_in_place"));
 	}
 
     void takeBallControl(IMessage rMessage) {
