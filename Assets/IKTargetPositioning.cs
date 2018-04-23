9f395af35a26b955c5de099857a9d58335c3c174
Dmitri Yourchev <d.yurchev@yandex.ru>
2018-04-23 19:16:02 +0300

    Implement IK Target Positioning

---
+++ Assets/IKTargetPositioning.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKTargetPositioning : MonoBehaviour {
+
+    public Vector3 playerDirection;
+    public Vector3 ballDirection;
+    public GameObject soccerWorld;
+    public GameObject soccerPlayer;
+    public GameObject soccerBall;
+
+    public Vector3 positionOffset;
+
+	// Use this for initialization
+	void Start () {
+        soccerPlayer = GameObject.FindGameObjectWithTag("Player");
+        soccerBall = GameObject.FindGameObjectWithTag("Ball");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerDirection = soccerPlayer.transform .position - gameObject.transform.position;
+        ballDirection = soccerBall.transform.position - gameObject.transform.position;
+        Debug.DrawRay(gameObject.transform.position, playerDirection, Color.yellow);
+        Debug.DrawRay(gameObject.transform.position, ballDirection, Color.yellow);
+
+        transform.position = soccerPlayer.transform.position + (soccerBall.transform.position - soccerPlayer.transform.position) + positionOffset;
+	}
+}
