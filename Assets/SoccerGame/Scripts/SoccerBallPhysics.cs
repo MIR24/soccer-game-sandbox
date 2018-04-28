bb43d1ec72519c3bf6d4ee68c3ba446d4bfb1a5a
Dmitri Yourchev <d.yurchev@yandex.ru>
2018-04-28 17:19:09 +0300

    Add dribbling direction interpolation
    Each time Player tries to kick the ball closely to Goal direction Ray.

--- Assets/SoccerGame/Scripts/SoccerBallPhysics.cs
+++ Assets/SoccerGame/Scripts/SoccerBallPhysics.cs
@@ -8,7 +8,13 @@ public class SoccerBallPhysics : MonoBehaviour {
     public float dribbleForceMultiplier = 1F;
     public Vector3 lstAccelerationPoint;
     public Vector3 lstAccelerationDirection;
+    public Vector3 goalDirection;
+    public Vector3 interimDirection;
+    public Vector3 accelerationVectorAlongWay;
+    public float lerpDirectionFactor = 0.3F;
     public Vector3 testForce;
+
+    public GameObject ballTarget;
 	// Use this for initialization
 	void Start () {
         ballPhysics = GetComponent<Rigidbody>();
@@ -16,7 +22,10 @@ public class SoccerBallPhysics : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-            Debug.DrawLine(lstAccelerationPoint, lstAccelerationDirection, Color.red);
+
+        Debug.DrawRay(transform.position, goalDirection, Color.yellow);
+        Debug.DrawRay(lstAccelerationPoint, lstAccelerationDirection, Color.green);
+        Debug.DrawRay(lstAccelerationPoint, accelerationVectorAlongWay, Color.red);
 	}
 
     void OnGUI()
@@ -32,17 +41,23 @@ public class SoccerBallPhysics : MonoBehaviour {
         if (collision.gameObject.transform.root.gameObject.tag == "Player") {
             lstAccelerationPoint = transform.position;
 
-            Vector3 accelerationVector = (lstAccelerationPoint - collision.gameObject.transform.root.gameObject.transform.position);
+            accelerationVectorAlongWay = (lstAccelerationPoint - collision.gameObject.transform.root.gameObject.transform.position);
 
-            //Remove Verticale difference between Player and Ball from acceleration
-            accelerationVector.y = 0;
+            //Remove vertical difference between Player and Ball positions from acceleration
+            accelerationVectorAlongWay.y = 0;
             //Normalize acceleration
-            accelerationVector = accelerationVector.normalized;
+            accelerationVectorAlongWay = accelerationVectorAlongWay.normalized;
             //Remove Verticale from Acceleration
-            accelerationVector.y = transform.position.y;
+            accelerationVectorAlongWay.y = transform.position.y;
+
 
             float objectMoveSpeed = collision.transform.root.gameObject.GetComponent<SoccerPlayer>().moveSpeed;
-            lstAccelerationDirection = accelerationVector * dribbleForceMultiplier;
+
+            goalDirection = ballTarget.transform.position - transform.position;
+            interimDirection = Vector3.Slerp(accelerationVectorAlongWay, goalDirection.normalized, lerpDirectionFactor);
+
+            lstAccelerationDirection = interimDirection * dribbleForceMultiplier;
+
             ballPhysics.AddForce(lstAccelerationDirection, ForceMode.VelocityChange);
             Debug.Log("Pushing ball forwards" + lstAccelerationDirection);
         }
